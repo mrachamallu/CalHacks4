c1c95faa8ad76548e67e04255f3e3b097a1cc5ad
mrachamallu <mrachamallu@gmail.com>
2017-10-07 03:39:23 -0700

    added controller

--- FudgetAPI/api/controllers/fudgetController.js
+++ FudgetAPI/api/controllers/fudgetController.js
@@ -4,53 +4,53 @@
 var mongoose = require('mongoose'),
   Item = mongoose.model('items');
 
-exports.list_all_tasks = function(req, res) {
+exports.list_all_items = function(req, res) {
   Item.find({}, function(err, items) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(items);
   });
 };
 
 
 
 
-exports.create_a_task = function(req, res) {
-  var new_task = new Task(req.body);
-  new_task.save(function(err, task) {
+exports.create_an_item = function(req, res) {
+  var new_item = new Item(req.body);
+  new_item.save(function(err, items) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(items);
   });
 };
 
 
-exports.read_a_task = function(req, res) {
-  Task.findById(req.params.taskId, function(err, task) {
+exports.read_an_item = function(req, res) {
+  Item.findById(req.params.taskId, function(err, items) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(items);
   });
 };
 
 
-exports.update_a_task = function(req, res) {
-  Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+exports.update_an_item = function(req, res) {
+  Item.findOneAndUpdate({_id: req.params.itemId}, req.body, {new: true}, function(err, items) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(items);
   });
 };
 
 
-exports.delete_a_task = function(req, res) {
+exports.delete_an_item = function(req, res) {
 
 
-  Task.remove({
-    _id: req.params.taskId
-  }, function(err, task) {
+  Item.remove({
+    _id: req.params.itemId
+  }, function(err, items) {
     if (err)
       res.send(err);
-    res.json({ message: 'Task successfully deleted' });
+    res.json({ message: 'Item successfully deleted' });
   });
 };
