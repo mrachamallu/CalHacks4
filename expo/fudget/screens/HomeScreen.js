f10132251330fd6945b03f4b9b943bdb50f01605
Krishna Gopal <gopalk@usc.edu>
2017-10-07 12:51:06 -0700

    Updating list budget

--- expo/fudget/screens/HomeScreen.js
+++ expo/fudget/screens/HomeScreen.js
@@ -1,112 +1,71 @@
+import React, { Component } from 'react';
 import Expo from 'expo';
-import React from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Image,
+  StyleSheet, 
+  Text, 
+  View, 
+  FlatList, 
+  TextInput, 
+  Button, 
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Image, 
+  Keyboard,
+  Platform,
 } from 'react-native';
 
-export default class App extends React.Component {
+import BudgetPart from './BudgetPart'
+
+export default class App extends Component {
   state = {
-    imageUri: null,
-    label: null,
+    budgets: []
+  }
+
+  keyExtractor = (topic, index) => topic.id;
+
+  loadPage = () => {
+    fetch('test.json')
+          .then((res) => res.json())
+          .then((data) => {
+            this.setState({
+              budgets: data.topics
+            });
+          })
+          .catch(function(error) {
+          console.log('There has been a problem with your fetch operation: ' + error.message);
+           // ADD THIS THROW error
+            throw error;
+          });
+
+    Keyboard.dismiss()
+  }
+
+  renderBudget = ({topic}) => {
+    return <BudgetPart 
+      type = {topic.type}
+      budget = {topic.budget}
+      moneySpent = {topic.moneySpent}
+      moneyLeft = {topic.moneyLeft}
+      percentage = {topic.percentage}
+      />
   }
 
   render() {
-    let imageView = null;
-    if (this.state.imageUri) {
-      imageView = (
-        <Image
-          style={{ width: 300, height: 300 }}
-          source={{ uri: this.state.imageUri }}
-        />
-      );
-    }
-
-    let labelView = null;
-    if (this.state.label) {
-      labelView = (
-        <Text style={{ margin: 5 }}>
-          {this.state.label}
-        </Text>
-      );
-    }
-
     return (
       <View style={styles.container}>
-        {imageView}
-        {labelView}
-        <TouchableOpacity
-          style={{ margin: 5, padding: 5, backgroundColor: '#ddd' }}
-          onPress={this._pickImage}>
-          <Text>take a picture!</Text>
-        </TouchableOpacity>
+        <Button title="Load" onPress={this.loadPage} color='#000000'/>
+        <FlatList
+          data={this.state.budgets}
+          keyExtractor={this.keyExtractor}
+          renderBudget={this.renderBudget}
+          style={styles.list}
+        />
       </View>
     );
   }
-
-  _pickImage = async () => {
-    const {
-      cancelled,
-      uri,
-      base64,
-    } = await Expo.ImagePicker.launchCameraAsync({
-      base64: true,
-    });
-    if (!cancelled) {
-      this.setState({
-        imageUri: uri,
-        label: '(loading...)',
-      });
-    }
-
-    const body = {
-      requests:[
-        {
-          image:{
-            content: base64,
-          },
-          features:[
-            {
-              type: 'TEXT_DETECTION',
-              maxResults: 100,
-            }
-          ]
-        },
-      ],
-    };
-
-    const key = 'AIzaSyCScDq8xvUnb1x4JDyt9zRHawD-imeyzuE';
-    const response_vis = await fetch(`https://vision.googleapis.com/v1/images:annotate?key=${key}`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    });
-    const parsed_vis = await response_vis.json();
-    this.setState({
-      label: parsed_vis.responses[0].textAnnotations[0].description,
-    });
-
-    // // send to custom api
-    // const res_db = await fetch('http://localhost:3000/receipts', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Accept': 'application/json',
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify(parsed_vision),
-    // });
-    // const parsed_db = await res_db.json();
-    // if(!res_db.fail){
-    //   // go to budget page
-    // }
-  }
 }
+
+  
 
 const styles = StyleSheet.create({
   container: {
@@ -114,6 +73,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  list: {
+    width: '100%'
   },
 });
 
