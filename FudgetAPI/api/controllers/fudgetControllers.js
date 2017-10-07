1ced1f6202b5a9f2354542c9c7a4c26679f2164a
Adithya Bellathur <bellathu@usc.edu>
2017-10-07 14:05:04 -0700

    finding apple

--- FudgetAPI/api/controllers/fudgetControllers.js
+++ FudgetAPI/api/controllers/fudgetControllers.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose'),
   Item = mongoose.model('Items');
 
 exports.list_all_items = function(req, res) {
-  Item.find({category: 'Groceries'}, function(err, items) {
+  Item.find({name: 'apple'}, function(err, items) {
     if (err)
       res.send(err);
     res.json(items);
