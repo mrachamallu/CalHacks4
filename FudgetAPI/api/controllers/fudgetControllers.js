00bacb8feef51b50597f9c2bfe236d5a0b067c57
Adithya Bellathur <bellathu@usc.edu>
2017-10-07 17:09:52 -0700

    More testing of receipts

--- FudgetAPI/api/controllers/fudgetControllers.js
+++ FudgetAPI/api/controllers/fudgetControllers.js
@@ -74,7 +74,7 @@ exports.delete_an_item = function(req, res) {
 //array of json objects
 var jsonOfItems = [];
 exports.read_receipt = function(req, res) {
-  var TA = req.body.textAnnotations;
+  var TA = JSON.parse(req).body.textAnnotations;
   console.log(TA);
   var storeLocation = TA[1].description; //the first element is always the store
   var dateOfPurchase = new Date(); //date of purchase
@@ -126,6 +126,6 @@ exports.read_receipt = function(req, res) {
   }
   var out = jsonOfItems;
 
-
+  res.setHeader("Content-Type:application/json");
   res.json(out);
 }
